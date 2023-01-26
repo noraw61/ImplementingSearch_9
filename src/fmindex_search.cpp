112eec8af293fdd9536c41ea53fc34d7f0cdf46d
Nora Wild <noraw61@zedat.fu-berlin.de>
2023-01-26 13:01:58 +0100

    100000 queries

--- src/fmindex_search.cpp
+++ src/fmindex_search.cpp
@@ -51,7 +51,7 @@ int main(int argc, char const* const* argv) {
     seqan3::configuration const cfg = seqan3::search_cfg::max_error_total{seqan3::search_cfg::error_count{0}};
 
     //!TODO here adjust the number of searches
-    queries.resize(10000); // will reduce the amount of searches
+    queries.resize(100000); // will reduce the amount of searches
 
 
     //!TODO !ImplementMe use the seqan3::search function to search
